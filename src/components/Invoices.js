4c434dc376bcc9a0715db2a76215b7a10f37c088
Sava Catalin <savactln0@gmail.com>
2024-09-27 13:24:59 +0300

    supplier filter and delete invoice

--- src/components/Invoices.js
+++ src/components/Invoices.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import "../styles/Invoices.css";
-import { doc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Invoices({ invoices, fetchInvoices, fetchInvoicesHome }) {
+  const [filteredInvoices, setFilteredInvoices] = useState(invoices);
+  const [supplierFilter, setSupplierFilter] = useState("");
+
+  useEffect(() => {
+    // Filter invoices based on the supplier input
+    setFilteredInvoices(
+      invoices.filter((invoice) =>
+        invoice.supplier.toLowerCase().includes(supplierFilter.toLowerCase())
+      )
+    );
+  }, [invoices, supplierFilter]);
+
   const markAsPaid = async (invoiceId) => {
     // Show confirmation prompt
     const confirmPayment = window.confirm(
@@ -29,11 +42,53 @@ function Invoices({ invoices, fetchInvoices, fetchInvoicesHome }) {
     }
   };
 
+  const deleteInvoice = async (invoiceId) => {
+    // Show confirmation prompt
+    const confirmDelete = window.confirm(
+      "Sunteti sigur ca vreti sa stergeti aceasta factura?"
+    );
+
+    if (!confirmDelete) {
+      return; // Exit if the user clicks "Cancel"
+    }
+
+    const invoiceRef = doc(db, "invoices", invoiceId);
+
+    try {
+      // Delete the document from Firestore
+      await deleteDoc(invoiceRef);
+
+      // Re-fetch invoices after deletion
+      fetchInvoices();
+      fetchInvoicesHome();
+    } catch (error) {
+      console.error("Error deleting invoice:", error);
+    }
+  };
+
+  // Calculate the total sum of unpaid invoices
+  const totalUnpaidSum = filteredInvoices
+    .filter((invoice) => !invoice.paid)
+    .reduce((acc, invoice) => acc + Number(invoice.totalSum), 0);
+
   return (
     <div className="page-content">
-      <h2 className="page-title2">Toate facturile</h2>
+      <div className="invoices-flex">
+        <div className="page-title2 width">Toate facturile</div>
+        <div className="supplier-flex width">
+          <span className="supplier-text">Furnizor: </span>
+          <input
+            className="supplier-input"
+            value={supplierFilter}
+            onChange={(e) => setSupplierFilter(e.target.value)} // Update filter on input change
+          />
+        </div>
+        <div className="width align-right">
+          <b>De plata:</b> {totalUnpaidSum.toFixed(2)} LEI
+        </div>
+      </div>
       <ul className="invoice-list">
-        {invoices.map((invoice) => {
+        {filteredInvoices.map((invoice) => {
           // Convert issueDate and paymentDate from Firestore timestamp to readable format
           const issueDateFormatted = moment(invoice.issueDate.toDate()).format(
             "DD-MM-YYYY"
@@ -71,14 +126,18 @@ function Invoices({ invoices, fetchInvoices, fetchInvoicesHome }) {
                 <span>
                   <b>Total plata:</b> {invoice.totalSum} LEI
                 </span>
-                {!invoice.paid && (
-                  <div
-                    className="paid-button"
-                    onClick={() => markAsPaid(invoice.id)}
-                  >
-                    Am platit
-                  </div>
-                )}
+                <div className="delete-flex">
+                  {!invoice.paid && (
+                    <div
+                      className="paid-button"
+                      onClick={() => markAsPaid(invoice.id)}
+                    >
+                      Am platit
+                    </div>
+                  )}
+
+                  <DeleteIcon onClick={() => deleteInvoice(invoice.id)} />
+                </div>
               </div>
             </li>
           );
--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Notfound = () => {
-  return <div className="page-content">
-    Pagina nu exista.
-  </div>;
+  return <div className="page-content">Pagina nu exista.</div>;
 };
 
 export default Notfound;
